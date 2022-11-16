06723c9fdc63eeb660a7e02d256708d350254fc7
Kevin Liu <kevnnliu@gmail.com>
2022-11-16 11:40:43 -0800

    Resolved bug where parameter was mistakenly passed in as string

--- tests/unit_tests/metrics_controller_tests.py
+++ tests/unit_tests/metrics_controller_tests.py
@@ -38,9 +38,9 @@ def run_basic_metrics_controller_get_all_metrics_by_sensor_id_and_timestamp_for_
     fullness_sensor=session.query(FullnessSensor).first()
     usage_sensor=session.query(UsageSensor).first()
     weight_sensor=session.query(WeightSensor).first()
-    assert len(session.query(FullnessMetric).filter_by(sensor_id=fullness_sensor.sensor_id).all())==len(get_fullness_by_sensor_id_and_timestamp(f"{fullness_sensor.sensor_id}", datetime(1,1,1), datetime.now()))
-    assert len(session.query(UsageMetric).filter_by(sensor_id=usage_sensor.sensor_id).all())==len(get_usage_by_sensor_id_and_timestamp(f"{usage_sensor.sensor_id}", datetime(1,1,1), datetime.now()))
-    assert len(session.query(WeightMetric).filter_by(sensor_id=weight_sensor.sensor_id).all())==len(get_weight_by_sensor_id_and_timestamp(f"{weight_sensor.sensor_id}", datetime(1,1,1), datetime.now()))
+    assert len(session.query(FullnessMetric).filter_by(sensor_id=fullness_sensor.sensor_id).all())==len(get_fullness_by_sensor_id_and_timestamp(fullness_sensor.sensor_id, datetime(1,1,1), datetime.now()))
+    assert len(session.query(UsageMetric).filter_by(sensor_id=usage_sensor.sensor_id).all())==len(get_usage_by_sensor_id_and_timestamp(usage_sensor.sensor_id, datetime(1,1,1), datetime.now()))
+    assert len(session.query(WeightMetric).filter_by(sensor_id=weight_sensor.sensor_id).all())==len(get_weight_by_sensor_id_and_timestamp(weight_sensor.sensor_id, datetime(1,1,1), datetime.now()))
 
 
 def run_basic_metrics_controller_get_recent_metrics_by_sensor_id_and_timestamp_for_all_metric_types():
@@ -49,26 +49,26 @@ def run_basic_metrics_controller_get_recent_metrics_by_sensor_id_and_timestamp_f
     fullness_sensor=session.query(FullnessSensor).first()
     current_time = datetime.now()
     session.add(FullnessMetric(fullness=-1, sensor_id=fullness_sensor.sensor_id, timestamp = current_time))
-    fullness_metrics=get_fullness_by_sensor_id_and_timestamp(f"{fullness_sensor.sensor_id}",current_time,datetime.now())
+    fullness_metrics=get_fullness_by_sensor_id_and_timestamp(fullness_sensor.sensor_id,current_time,datetime.now())
     assert len(fullness_metrics) == 1, "Asserts that get_fullness_by_sensor_id_and_timestamp only returns the most recent fullness metric"
     assert fullness_metrics[0]["fullness"]==-1, "Asserts that the most recent mettric returned by get_fullness_by_sensor_id_and_timestamp is correct"
 
     usage_sensor=session.query(UsageSensor).first()
     current_time = datetime.now()
     session.add(UsageMetric(used_rate=-1, sensor_id=usage_sensor.sensor_id, timestamp = current_time))
-    usage_metrics=get_usage_by_sensor_id_and_timestamp(f"{usage_sensor.sensor_id}",current_time,datetime.now())
+    usage_metrics=get_usage_by_sensor_id_and_timestamp(usage_sensor.sensor_id,current_time,datetime.now())
     assert len(usage_metrics) == 1, "Asserts that get_usage_by_sensor_id_and_timestamp only returns the most recent usage metric"
     assert usage_metrics[0]["used_rate"]==-1, "Asserts that the most recent mettric returned by get_usage_by_sensor_id_and_timestamp is correct"
 
     weight_sensor=session.query(WeightSensor).first()
     current_time = datetime.now()
     session.add(WeightMetric(weight=-1, sensor_id=weight_sensor.sensor_id, timestamp = current_time))
-    weight_metrics=get_weight_by_sensor_id_and_timestamp(f"{weight_sensor.sensor_id}",current_time,datetime.now())
+    weight_metrics=get_weight_by_sensor_id_and_timestamp(weight_sensor.sensor_id,current_time,datetime.now())
     assert len(weight_metrics) == 1, "Asserts that get_weight_by_sensor_id_and_timestamp only returns one weight metric"
     assert weight_metrics[0]["weight"]==-1, "Asserts that the most recent mettric returned by get_fullness_by_sensor_id_and_timestamp is correct"
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     run_basic_metrics_controller_get_all_for_all_metric_types()
     run_basic_metrics_controller_get_all_metrics_by_sensor_id_and_timestamp_for_all_metric_types()
     run_basic_metrics_controller_get_recent_metrics_by_sensor_id_and_timestamp_for_all_metric_types()
